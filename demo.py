63fba532dc7b8ad38ac0b5dee5543a1c4ba26429
MYounus-Codes <myounushere@gmail.com>
2025-12-19 11:35:24 +0500

    Demo and very basic server setup for biggners is added

---
+++ demo.py
@@ -0,0 +1,10 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("Demo Server")
+@mcp.tool()
+def my_info(name: str, age: int) -> str:
+    """Echo the input message"""
+    return f"Name: {name}, Age: {age}"
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
